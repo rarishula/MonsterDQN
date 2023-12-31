9ca5057bfe188096339db59529f68cd6cd2c4652
rarishula <67272393+rarishula@users.noreply.github.com>
2023-12-31 11:36:01 +0900

    Add files via upload

---
+++ Agent.py
@@ -0,0 +1,29 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from stable_baselines3 import DQN
+from stable_baselines3.dqn.policies import MlpPolicy
+from GameEnvironment import GameEnvironment
+
+# Create custom environment
+env = GameEnvironment()
+
+# Construct the DQN agent
+model = DQN(MlpPolicy, env, learning_rate=1e-3, batch_size=32, buffer_size=50000, 
+            learning_starts=10, gamma=0.95, target_update_interval=1000, 
+            train_freq=4, gradient_steps=1, exploration_fraction=0.1, 
+            exploration_initial_eps=1.0, exploration_final_eps=0.01)
+            
+# Execute training
+model.learn(total_timesteps=50000)
+
+# Execute testing
+episodes = 5
+for episode in range(episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        env.render()
