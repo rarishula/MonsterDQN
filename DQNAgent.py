32bd0fc8280e3c9de4d7122c2cc639fb55748b84
rarishula <67272393+rarishula@users.noreply.github.com>
2024-01-04 11:00:30 +0900

    Update DQNAgent.py

--- DQNAgent.py
+++ DQNAgent.py
@@ -7,7 +7,7 @@ from stable_baselines3.dqn.policies import MlpPolicy
 from GameEnvironment import GameEnvironment
 
 class DQNAgentTrainer:
-    def __init__(self, env, learning_rate=1e-3, batch_size=32, buffer_size=5000, 
+    def __init__(self, env, learning_rate=1e-3, batch_size=32, buffer_size=50000, 
                  learning_starts=10, gamma=0.95, target_update_interval=1000, 
                  train_freq=4, gradient_steps=1, exploration_fraction=0.8, 
                  exploration_initial_eps=1.0, exploration_final_eps=0.01):
@@ -21,7 +21,7 @@ class DQNAgentTrainer:
                          exploration_initial_eps=exploration_initial_eps, 
                          exploration_final_eps=exploration_final_eps)
 
-    def train(self, total_timesteps=5000):
+    def train(self, total_timesteps=50000):
         self.model.learn(total_timesteps=total_timesteps)
 
     def test(self, episodes=1000):
@@ -37,7 +37,8 @@ class DQNAgentTrainer:
 
 def main(damage_reward_range = 50, front_monster_advantage_reward_range = 10):
     # カスタム環境の作成
-    env = GameEnvironment(damage_reward_range, front_monster_advantage_reward_range)
+    #env = GameEnvironment(damage_reward_range, front_monster_advantage_reward_range)
+    env = GameEnvironment()
 
     # DQNAgentTrainerクラスのインスタンス化
     agent_trainer = DQNAgentTrainer(env)
